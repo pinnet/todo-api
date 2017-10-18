f47e7ed593571bc77edd1c468127bdeef33b63e9
Danny Arnold <danny@dannyarnold.com>
2017-10-19 00:04:47 +0100

    fix

--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ var {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
 
 var app = express();
 const port = process.env.PORT || 3000;

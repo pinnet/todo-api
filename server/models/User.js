313c3f455b726ffdbdf531add54e313774d58c08
Danny Arnold <danny@dannyarnold.com>
2017-10-21 10:30:15 +0100

    added shadow config

--- server/models/User.js
+++ server/models/User.js
@@ -37,7 +37,7 @@ UserSchema.statics.findByToken = function(token){
   var User = this;
   var decoded;
   try{
-    decoded = jwt.verify(token,'ABC123');
+    decoded = jwt.verify(token,process.env.JWT_SECRET);
   } catch (e) {
    return Promise.reject();
   }
@@ -77,7 +77,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function (){
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(),access},'ABC123').toString();
+  var token = jwt.sign({_id: user._id.toHexString(),access},process.env.JWT_SECRET).toString();
   user.tokens.push({access,token});
   return user.save().then(() => {
     return token;    
